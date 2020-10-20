de508953fb826eb6bce2384bda6375cb448dd698
Laura Gonzalez <gonzale5@sheridancollege.ca>
2020-10-19 23:43:56 -0400

    dao

--- app/src/main/java/sheridan/gonzale5/assignment2/database/GameScoreDao.kt
+++ app/src/main/java/sheridan/gonzale5/assignment2/database/GameScoreDao.kt
@@ -1,5 +1,19 @@
 package sheridan.gonzale5.assignment2.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+
+@Dao
 interface GameScoreDao {
+
+    //getall
+    @Query("SELECT * FROM game_score ORDER BY id")
+    fun getAll() : LiveData<List<GameScore>>
+
+    //deleteall
+    @Query("DELETE FROM game_score")
+    suspend fun deleteAll()
 
 }
